88e86aeb958574b32384583c0501682278b9ad08
Grant Kiely <1948935+gkiely@users.noreply.github.com>
2024-09-08 18:05:55 -0500

    working page

--- main.d.ts
+++ main.d.ts
@@ -14,10 +14,216 @@ type Token = {
   scope: string;
 };
 
+type GapiPromise<T> = {
+  then: (o: (o: T) => unknown) => T;
+  getPromise: () => Promise<T>;
+};
+type PermissionRole = 'owner' | 'organizer' | 'fileOrganizer' | 'writer' | 'commenter' | 'reader';
+type Permission =
+  | {
+      id: 'anyoneWithLink' | 'anyone';
+      role: PermissionRole;
+      type: 'anyone';
+    }
+  | {
+      id: string;
+      role: PermissionRole;
+      type: 'user' | 'group';
+      emailAddress: string;
+      displayName: string;
+      photoLink: string;
+      deleted?: boolean;
+    }
+  | {
+      id: string;
+      role: PermissionRole;
+      type: 'domain';
+      displayName: string;
+    };
+
+type BaseFile = {
+  id: string;
+  name: string;
+  driveId?: string;
+  explicitlyTrashed: boolean;
+  iconLink?: string;
+  parents?: [string]; // Parents is optional for anyoneWithLink files
+  webViewLink: string;
+  webContentLink: string;
+  lastModifyingUser?: {
+    displayName: string;
+    photoLink?: string;
+  };
+  createdTime: string;
+  modifiedTime: string;
+  owners?: {
+    displayName: string;
+    photoLink?: string;
+    emailAddress: string;
+  }[];
+  ownedByMe: boolean;
+  permissions?: Permission[];
+  properties?: {
+    animations?: 'true' | 'false';
+    color?: string;
+    fontColor?: string;
+    highlightTextMenu?: 'true' | 'false';
+    showPrintButton?: 'true' | 'false';
+    index?: string | null;
+    parentId?: string | null; // Used to check if index is valid
+    YNAW?: 'folder';
+    unicode?: string;
+    logoId?: string;
+    logoURL?: string;
+    sheetBorder?: 'true' | 'false';
+    showPageTitle?: 'true' | 'false';
+    collapsedMenu?: 'true' | 'false';
+    showViewButtons?: 'true' | 'false';
+    userMenu?: 'true' | 'false';
+    showCreateWikiButton?: 'true' | 'false';
+  };
+  capabilities?: {
+    canEdit: boolean;
+    canDelete: boolean;
+  };
+  thumbnailLink?: string;
+  imageMediaMetadata?: {
+    width: number;
+    height: number;
+  };
+  size: number;
+};
+
+type Document = BaseFile & {
+  mimeType: 'application/vnd.google-apps.document';
+};
+
+// If you make updates here be sure to update server/types.ts
+type DriveFile =
+  // Shared drive
+  | (Omit<BaseFile, 'parents'> & {
+      mimeType: 'application/vnd.google-apps.folder';
+      driveId: string;
+      parents?: [string]; // Parents is optional for shared drives
+    })
+  | Document // Allows type narrowing
+  | (BaseFile & {
+      mimeType:
+        | 'application/vnd.google-apps.document'
+        | 'application/vnd.google-apps.folder'
+        | 'application/vnd.google-apps.drawing'
+        | 'application/vnd.google-apps.spreadsheet'
+        | 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        | 'application/vnd.google-apps.presentation'
+        | 'application/vnd.ms-powerpoint'
+        | 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+        | 'application/vnd.google-apps.form'
+        | 'video/mp4'
+        | 'video/quicktime'
+        | 'text/markdown'
+        | 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        | 'application/pdf'
+        | 'application/x-iwork-numbers-sffnumbers'
+        | 'image/jpeg'
+        | 'text/plain'
+        | 'text/html'
+        | 'image/png'
+        | 'image/vnd.adobe.photoshop'
+        | 'application/postscript'
+        | 'image/svg+xml'
+        | 'application/octet-stream'
+        | 'application/json'
+        | 'application/x-iwork-pages-sffpages'
+        | 'application/vnd.openxmlformats-officedocument.presentationml.slideshow'
+        | 'text/csv'
+        | 'application/zip'
+        | 'application/msword'
+        | 'application/x-ms-shortcut';
+    })
+  // Shortcut
+  | (BaseFile & {
+      mimeType: 'application/vnd.google-apps.shortcut';
+      shortcutDetails: {
+        targetId: string;
+        targetMimeType: Exclude<DriveFile['mimeType'], 'application/vnd.google-apps.shortcut'>;
+        target?: DriveFile;
+      };
+    });
+type ExportMimeType =
+  | 'text/html'
+  | 'text/plain'
+  | 'text/csv'
+  | 'text/markdown'
+  | 'application/zip'
+  | 'application/pdf'
+  | 'application/rtf'
+  | 'application/vnd.oasis.opendocument.text'
+  | 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+  | 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+  | 'application/vnd.oasis.opendocument.spreadsheet'
+  | 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+  | 'application/vnd.oasis.opendocument.presentation'
+  | 'application/vnd.openxmlformats-officedocument.presentationml.slideshow'
+  | 'application/vnd.google-apps.script+json'
+  | 'application/epub+zip'
+  | 'text/tab-separated-values'
+  | 'image/jpeg'
+  | 'image/png'
+  | 'image/svg+xml';
+
+type GapiResponse<T> = GapiPromise<{
+  body: string;
+  headers: {
+    'cache-control': 'no-cache, no-store, max-age=0, must-revalidate';
+    'content-type': 'application/json; charset=UTF-8';
+    'content-encoding': 'gzip';
+    'content-length': string;
+    date: string;
+    expires: string;
+    pragma: 'no-cache';
+    server: 'ESF';
+    vary: 'Origin, X-Origin';
+  };
+  status: 200;
+  statusText: null;
+  result: T;
+}>;
+
+type Params = {
+  fields?: string;
+  supportsAllDrives: true;
+  includeItemsFromAllDrives: true;
+};
+
+type DriveAPI = {
+  Get: {
+    options: {
+      path: `https://www.googleapis.com/drive/v3/files/${string}`;
+      params?: Params & {
+        alt?: 'media';
+      };
+    };
+    response: GapiResponse<DriveFile>;
+  };
+  Export: {
+    options: {
+      path: `https://www.googleapis.com/drive/v3/files/${string}/export`;
+      params: Params & {
+        mimeType: ExportMimeType;
+      };
+    };
+    response: GapiResponse<{ body: string }>;
+  };
+};
+
 // declare global gapi
 declare const gapi: {
   client: {
     getToken: () => Token | null;
+    request: {
+      (options: DriveAPI['Get']['options']): DriveAPI['Get']['response'];
+      (options: DriveAPI['Export']['options']): DriveAPI['Export']['response'];
+    };
   };
 };
 
--- src/Page.js
+++ src/Page.js
@@ -3,15 +3,57 @@ import { useState, useEffect } from 'https://esm.sh/preact/hooks';
 import register from 'https://esm.sh/preact-custom-element';
 import { html } from 'https://esm.sh/htm/preact';
 
+// 4. Fetch content
+// https://developers.google.com/drive/api/reference/rest/v3/files/export
+const fetchContent = async ({ id = '' }) => {
+  const response = await gapi.client.request({
+    path: `https://www.googleapis.com/drive/v3/files/${id}/export`,
+    params: {
+      fields: '*',
+      mimeType: 'text/html',
+      supportsAllDrives: true,
+      includeItemsFromAllDrives: true,
+    },
+  });
+
+  return response.body;
+};
+
+const fetchFile = async ({ id = '' }) => {
+  const response = await gapi.client.request({
+    path: `https://www.googleapis.com/drive/v3/files/${id}`,
+    params: {
+      fields: '*',
+      supportsAllDrives: true,
+      includeItemsFromAllDrives: true,
+    },
+  });
+
+  return response.result;
+};
+
 const Page = () => {
-  // 4. Fetch content
+  // 4.1 Fetch content
+  const [content, setContent] = useState('');
+  const [error, setError] = useState('');
+
+  /**
+   * Fetch file
+   * @type {[{id: string; name: string} | undefined, function]}
+   */
+  const [file, setFile] = useState();
+
+  useEffect(() => {
+    fetchContent({ id: '1VDTyWNVCspD2mMC-IQQUtgf8HyLUe37C6G87TheqJhI' }).then(setContent).catch(setError);
+    fetchFile({ id: '1VDTyWNVCspD2mMC-IQQUtgf8HyLUe37C6G87TheqJhI' }).then(setFile).catch(setError);
+  }, []);
 
   // 1. Static content
   return html`
     <div class="Page">
-      <h1>Page title</h1>
+      <h1>${file?.name}</h1>
       <p class="content">
-        Here is my google docs content
+      ${content ? html`<div dangerouslySetInnerHTML=${{ __html: content }}></div>` : ''}
       </p>
     </div>
   `;
