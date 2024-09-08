8e884648cc0b01f10fb313f6ba513206e0f5d7b3
Grant Kiely <1948935+gkiely@users.noreply.github.com>
2024-09-08 16:27:00 -0500

    initial commit

---
+++ main.d.ts
@@ -0,0 +1,22 @@
+declare module 'https://esm.sh/preact-custom-element' {
+  export { default } from '@types/preact-custom-element';
+}
+declare module 'https://esm.sh/preact/hooks' {
+  export * from 'preact/hooks';
+}
+declare module 'https://esm.sh/htm/preact' {
+  export * from 'htm/preact';
+}
+
+type Token = {
+  access_token: string;
+  expires_in: number;
+  scope: string;
+};
+
+// declare global gapi
+declare const gapi: {
+  client: {
+    getToken: () => Token | null;
+  };
+};
---
+++ scripts/install-latest.ts
@@ -0,0 +1,23 @@
+import packageJSON from '../package.json';
+
+const entries = [...Object.entries(packageJSON.dependencies), ...Object.entries(packageJSON.devDependencies)];
+const betaAndRcDependencies = entries.filter(([_, v]) => v.includes('beta') || v.includes('rc')).map(([k]) => k);
+const fileDependencies = entries.filter(([_, v]) => v.includes('file:')).map(([k]) => k);
+
+const blacklist: string[] = [
+  ...betaAndRcDependencies,
+  ...fileDependencies,
+];
+
+const deps =
+  `${Object.keys({
+    ...packageJSON.dependencies,
+    ...packageJSON.devDependencies,
+  })
+    .filter((k) => !blacklist.includes(k))
+    // .join('@latest ')}@latest`;
+    .join('@latest ')}@latest`;
+
+// Output to shell
+// eslint-disable-next-line no-console
+console.log(deps);
