1b0ece2539e11209edd8b0753e9330325bdb427c
Grant Kiely <1948935+gkiely@users.noreply.github.com>
2024-09-09 16:14:58 -0500

    Added code block support

--- src/Page.js
+++ src/Page.js
@@ -21,19 +21,24 @@ const whitelist = ['youtube.com', 'loom.com', 'google.com', 'docs.google.com', '
  */
 const parseContent = (html = '') => {
   console.log(html);
-  return html.replace(
-    /(&lt;div((?!&gt;).)*&gt;)?&lt;iframe((?!&gt;).)*&gt;(&lt;\/iframe&gt;)(&lt;\/div&gt;)?/g,
-    (str) => {
-      const match = str.replace(/&quot;/g, '"').match(/src="([^"]+)"/)?.[1];
-      const url = match ? getDomain(match) : undefined;
-      if (url && whitelist.includes(url)) {
-        return str
-          .replace(/&lt;/g, '<')
-          .replace(/&gt;/g, '>')
-          .replace(/&quot;/g, '"');
-      }
-      return str;
-    }
+  return (
+    html
+      .replace(/(&lt;div((?!&gt;).)*&gt;)?&lt;iframe((?!&gt;).)*&gt;(&lt;\/iframe&gt;)(&lt;\/div&gt;)?/g, (str) => {
+        const match = str.replace(/&quot;/g, '"').match(/src="([^"]+)"/)?.[1];
+        const url = match ? getDomain(match) : undefined;
+        if (url && whitelist.includes(url)) {
+          return str
+            .replace(/&lt;/g, '<')
+            .replace(/&gt;/g, '>')
+            .replace(/&quot;/g, '"');
+        }
+        return str;
+      })
+      // Code block
+      .replace(/<p[^>]*><span[^>]*>&#60419;[^<]*<\/span>(?:(?!&#60418;).)*&#60418;<\/span><\/p>/g, (str) => {
+        const output = str.replace('&#60419;', '').replace('&#60418;', '');
+        return /*html*/ `<div class="code">${output}</div>`;
+      })
   );
 };
 
