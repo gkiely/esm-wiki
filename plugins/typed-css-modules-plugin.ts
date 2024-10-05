79b2b6855d784b27ee220f649101f39627741ba5
Grant Kiely <1948935+gkiely@users.noreply.github.com>
2024-10-05 14:24:42 -0500

    updated typed css module tracking to work on startup

--- plugins/typed-css-modules-plugin.ts
+++ plugins/typed-css-modules-plugin.ts
@@ -2,9 +2,32 @@ import fs from 'node:fs';
 import { DtsCreator } from 'typed-css-modules/lib/dts-creator.js';
 import type { Plugin, UserConfig } from 'vite';
 
+const creator = new DtsCreator({ camelCase: true });
+const createDTSFile = async (path: string) => {
+  try {
+    const content = await creator.create(path, undefined, true);
+    await content.writeFile();
+  } catch {
+    console.error('Failed to generate .d.ts file for', path);
+  }
+};
+
+// Recursively get all file paths in a directory
+const walk = (dir: string, ext: string, filelist: string[] = []) => {
+  const files = fs.readdirSync(dir);
+  files.forEach((file) => {
+    const path = `${dir}/${file}`;
+    if (fs.statSync(path).isDirectory()) {
+      walk(path, ext, filelist);
+    } else if (path.endsWith(ext)) {
+      filelist.push(path);
+    }
+  });
+  return filelist;
+};
+
 // Inspired by: https://github.com/jhwz/vite-plugin-typed-css-modules
 function plugin(): Plugin {
-  const creator = new DtsCreator({ camelCase: true });
   return {
     name: 'typed-css-modules',
     config: () => {
@@ -18,14 +41,13 @@ function plugin(): Plugin {
       return config;
     },
     configureServer: (server) => {
+      console.log('Configuring server');
+      const files = walk('src', '.module.css');
+      files.forEach(createDTSFile);
+
       server.watcher.on('change', async (path) => {
         if (!path.endsWith('.module.css')) return;
-        try {
-          const content = await creator.create(path, undefined, true);
-          await content.writeFile();
-        } catch {
-          console.error('Failed to generate .d.ts file for', path);
-        }
+        createDTSFile(path);
       });
       server.watcher.on('unlink', (path) => {
         if (!path.endsWith('.module.css')) return;
